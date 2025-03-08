f44263d3c22a981f28331ad3e6fac20d014efe0f
Vladislav Naftolin <vnaftolin@mail.ru>
2025-03-08 15:00:30 +0300

    feat: implement getDayName function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -25,19 +25,27 @@ function getTime(date) {
   return `${hours}:${minutes}:${seconds}`;
 }
 
-/**
- * Returns the name of the day of the week for a given date string.
- *
- * @param {string} date - date and time.
- * @return {string} the name of the day of the week
- *
- * @example:
- * '01 Jan 1970 00:00:00 UTC' => 'Thursday'
- * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
- * '2024-01-30T00:00:00.000Z' => 'Tuesday'
- */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const newDate = new Date(date);
+
+  switch (newDate.getDay()) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    default:
+      return '';
+  }
 }
 
 /**
